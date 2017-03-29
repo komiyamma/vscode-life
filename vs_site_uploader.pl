a38020974402abf3b301de1d18b68a30211a799a
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-03-30 03:58:27 +0900

    Perlに変更

---
+++ vs_site_uploader.pl
@@ -0,0 +1,12 @@
+# VSCodeサイトへのFTP
+my $filename = $ARGV[0];
+if ($filename =~ /\bVS_/) {
+	print "upload start...\n";
+	$cmd = sprintf('cmd /C C:\usr\nextftp\NEXTFTP.EXE $Host13 -local="C:\usr\web\VSCode" -upload=%s -quit -nosound -minimize', $filename );
+	print $cmd . "\n";
+	print `$cmd`;
+
+	print "upload complete!\n";
+} else {
+	print "error";
+}
