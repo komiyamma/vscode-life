42351acc48f778b764cff4aa40146ef52b740032
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-04-04 03:18:21 +0900

    シンボリックリンクかどうかのチェック用プログラム

---
+++ win-general-app/WinCheckSymbolicLink.src/WinCheckSymbolicLink.src/WinCheckSymbolicLink/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace WinCheckSymbolicLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) { 
+                var target = args[0];
+                if (Directory.Exists(target)) {
+                    FileAttributes attrs = File.GetAttributes(target);
+                    if ((attrs & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint)
+                    {
+                        Console.WriteLine("true");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("false");
+            return;
+        }
+    }
+}
