82472c4da8a63b249d3c595d98b1de1b5bc73897
vscode-life <akitsugu-komiyama@docomo.ne.jp>
2017-04-08 22:12:08 +0900

    require ではなくimport

--- win-vscode-app/pl2exe/src/extension.ts
+++ win-vscode-app/pl2exe/src/extension.ts
@@ -2,8 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API Import the module
 // and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
-let process_exec = require("child_process").exec;
+import * as child_process from 'child_process';
 
 // this method is called when your extension is activated your extension is
 // activated the very first time the command is executed
