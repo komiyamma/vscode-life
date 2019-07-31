4beafe338e60dd9aad975bdab6f8fc73ce7b6952
komiyamma <akitsugu-komiyama@docomo.ne.jp>
2019-07-31 21:11:45 +0900

    サイトもアップ

---
+++ VS_LIB_pagetree.php
@@ -0,0 +1,30 @@
+<?php
+$content_hash = [
+    'VS_top' =>
+        [ 'html'=>'VS_top.html', 'title'=>'トップページ' , 'dir'=>'初期設置'],
+
+    'VS_setup_overview' =>
+        [ 'html'=>'VS_setup_overview.html', 'title'=>'VSCode関連リンク' , 'dir'=>'セットアップ'],
+
+    'VS_keybindings' =>
+        [ 'html'=>'VS_keybindings.html', 'title'=>'既定のキーバインド' , 'dir'=>'セットアップ'],
+
+	'VS_extension_location' =>
+        [ 'html'=>'VS_extension_location.html', 'title'=>'自作のextensionの配置場所' , 'dir'=>'拡張機能'],
+
+    'VS_font_rdd' =>
+        [ 'html'=>'VS_font_rdd.html', 'title'=>'Rictyフォント ' , 'dir'=>'フォント'],
+
+    'VS_book_ts' =>
+        [ 'html'=>'VS_book_ts.html', 'title'=>'TypeScript関連のお薦め電子書籍' , 'dir'=>'リンク'],
+
+    'VS_link' =>
+        [ 'html'=>'VS_link.html', 'title'=>'VSCode関連リンク' , 'dir'=>'リンク']
+];
+
+
+
+$filetime_hash = [
+
+];
+?>
